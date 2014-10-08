92982552d20be2fede6ad9998793ff7924bc9407
Torey Hickman <torey@toreyhickman.com>
2014-10-08 11:12:20 -0400

    Add before filter to ensure signin
    Filter added to application controller.  Filter skipped in session controller.
    
    Helper methods also refactored to pass session along with get request.  This was a technique that I used to test the before filter and wanted the styles to match.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :ensure_signin
+
   helper_method :signed_in?, :current_token
 
   def signed_in?
@@ -11,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def current_token
     session[:github_token]
+  end
+
+  def ensure_signin
+    redirect_to signin_path unless signed_in?
   end
 end
