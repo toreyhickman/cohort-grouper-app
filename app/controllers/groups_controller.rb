7829c075c5aca1bff080af139512dd2e153d651b
Torey Hickman <torey@toreyhickman.com>
2014-10-23 19:51:38 -0500

    GroupsController#new provides suggested groups
    When the user click the "Make New Groups" link, a new Grouping is created.  Then the user is redirected to the new_cohort_grouping_group_path.  This triggers the GroupsController#new action.
    
    This action ...
      1. Updates the students that belong to the cohort.  (This should probably be refactored somewhere else.)
    
      2. Creates proposed groups that will be presented to the user as a sort of form stand-in.  If the user likes the groups, the user will be able to submit the proposed groups to the GroupsController#create action.
    
    The user interaction portions still need to be built out.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,10 @@
 class GroupsController < ApplicationController
   def new
     @cohort = Cohort.find_by_slug(params[:cohort_id])
+
+    student_list = UpdateCohortStudentsList.call(@cohort)
+    past_groups = student_list.map(&:groups).reduce(:+)
+
+    @proposed_groups = Garoupa.make_groups student_list.to_a, { past_groups: past_groups }
   end
 end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe GroupsController do
   let(:cohort) { create(:cohort) }
   let(:grouping) { Grouping.create(cohort_id: cohort.id) }
+  let(:student) { create(:student) }
 
   context "without signing in" do
     describe "GET#new" do
@@ -16,12 +17,26 @@ describe GroupsController do
   context "user signed in" do
     before(:each) do
       session[:github_token] = :token
+      stub_dbc_api_cohort_response
     end
 
     describe "GET#new" do
       it "collects cohort into @cohort " do
         get :new, { cohort_id: cohort.slug, grouping_id: grouping.id }
         expect(assigns(:cohort)).to eq cohort
+      end
+
+      it "calls for updating the cohort's students list" do
+        expect(UpdateCohortStudentsList).to receive(:call).with(cohort) { [student] }
+
+        get :new, { cohort_id: cohort.slug, grouping_id: grouping.id }
+      end
+
+      it "collects proposed groups into @proposed_groups" do
+        allow(Garoupa).to receive(:make_groups) { :garoupa_object }
+
+        get :new, { cohort_id: cohort.slug, grouping_id: grouping.id }
+        expect(assigns(:proposed_groups)).to eq :garoupa_object
       end
 
       it "renders the new template" do
